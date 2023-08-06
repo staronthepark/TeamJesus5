176a5b0c2294a287a26d0df150e8efd9623135e4
RuuNee <bio8617@gmail.com>
2023-08-06 15:30:21 +0900

    .

---
+++ Source/SERVAME/Monster/KinghtMonster.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "KinghtMonster.h"
+
+AKinghtMonster::AKinghtMonster()
+{
+
+}
---
+++ Source/SERVAME/Monster/KinghtMonster.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "..\BaseCharacter.h"
+#include "KinghtMonster.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SERVAME_API AKinghtMonster : public ABaseCharacter
+{
+	GENERATED_BODY()
+public:
+	AKinghtMonster();
+};
