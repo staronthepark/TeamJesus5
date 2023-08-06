d0a22c79c1d0c8c98a64da6b82590acb24ad00a6
RuuNee <bio8617@gmail.com>
2023-08-06 17:03:25 +0900

    .

---
+++ Source/SERVAME/Monster/Knight/KinghtMonster.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "..\..\BaseCharacter.h"
+#include "..\..\Player\PlayerCharacter.h"
+#include "KinghtMonster.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SERVAME_API AKinghtMonster : public ABaseCharacter
+{
+	GENERATED_BODY()
+public:
+	AKinghtMonster();
+};
---
+++ Source/SERVAME/Monster/Knight/KnightAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "..\..\BaseAnimInstance.h"
+#include "KnightAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SERVAME_API UKnightAnimInstance : public UBaseAnimInstance
+{
+	GENERATED_BODY()
+	
+};
