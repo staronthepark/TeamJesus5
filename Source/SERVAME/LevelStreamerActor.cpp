bed398949ccfe27ab91ce5f560c44a301872575f
staronthepark <asdffg6934@naver.com>
2023-08-21 19:04:45 +0900

    .

--- Source/SERVAME/LevelStreamerActor.cpp
+++ Source/SERVAME/LevelStreamerActor.cpp
@@ -17,7 +17,7 @@ void ALevelStreamerActor::BeginPlay()
 	OverlapVolume->OnComponentEndOverlap.AddDynamic(this, &ALevelStreamerActor::OverlapEnds);
 	OverlapVolume->OnComponentEndOverlap.AddDynamic(this, &ALevelStreamerActor::OverlapEndsLoad);
 
-	if (LevelToLoad == "2-2Map")return;
+	if (LevelToLoad == "2-2Map" || LevelToLoad == "MainHall")return;
 	for (int32 i = 0; i < MonsterList.Num(); i++)
 	{
 		MonsterList[i]->SetActive(false);
