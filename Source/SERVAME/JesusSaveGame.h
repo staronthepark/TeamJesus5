8a7be71c77ae6e4e8eeda27c13d153563159b264
parkjin <104075813+staronthepark@users.noreply.github.com>
2023-09-06 13:44:03 +0900

    .

--- Source/SERVAME/JesusSaveGame.h
+++ Source/SERVAME/JesusSaveGame.h
@@ -6,27 +6,6 @@
 #include "GameFramework/SaveGame.h"
 #include "JesusSaveGame.generated.h"
 
-USTRUCT(BlueprintType)
-struct FPlayerLocStruct
-{
-	GENERATED_USTRUCT_BODY()
-
-public:
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "SaveData")
-	FVector PlayerLoc;
-};
-
-USTRUCT(BlueprintType)
-struct FPlayerRotStruct
-{
-	GENERATED_USTRUCT_BODY()
-
-public:
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "SaveData")
-	FRotator PlayerRot;
-};
-
-
 UCLASS()
 class SERVAME_API UJesusSaveGame : public USaveGame
 {
@@ -39,19 +18,19 @@ public:
 	int32 SaveIndex;	//Save game data file index
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "SaveData")
-	FPlayerLocStruct PlayerLocStruct;
+	FVector PlayerLoc;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "SaveData")
-	FPlayerRotStruct PlayerRotStruct;
+	FRotator PlayerRot;
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "SaveData")
-	int HealCount;
+	//UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "SaveData")
+	//	FPlayerCharacterDataStruct PlayerData;
+
 	
 public:
+	//void SavePlayerData(FPlayerCharacterDataStruct data);
 	void SaveLoc(FVector Vec);
 	void SaveRot(FRotator Rot);
-	void SaveHealCount(int Count);
-	void SaveSavePoint();
 	void Save(UJesusSaveGame* Instance);
-	void Load(UJesusSaveGame* LoadInstance);
+	UJesusSaveGame* Load(UJesusSaveGame* LoadInstance);
 };
