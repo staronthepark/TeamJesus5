c68550e7ae6c628db16b03a002291e3b1d8b7161
parkjin <104075813+staronthepark@users.noreply.github.com>
2023-09-06 17:35:30 +0900

    .

--- Source/SERVAME/FlowerActor.cpp
+++ Source/SERVAME/FlowerActor.cpp
@@ -4,17 +4,23 @@ AFlowerActor::AFlowerActor()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
+	ParentComp = CreateDefaultSubobject<USceneComponent>("SceneComp");
 	TriggerComp = CreateDefaultSubobject<UBoxComponent>("Trigger");
 	ChangeMeshComp = CreateDefaultSubobject<UStaticMeshComponent>("Static Mesh");
-	ChangeMeshComp->SetupAttachment(TriggerComp);
 	OriginMeshComp = CreateDefaultSubobject<UStaticMeshComponent>("Static Mesh2");
-	OriginMeshComp->SetupAttachment(TriggerComp);
+	NiagaraComp = CreateDefaultSubobject<UNiagaraComponent>("Particle Comp");
+
+	TriggerComp->SetupAttachment(ParentComp);
+	NiagaraComp->SetupAttachment(ParentComp);
+	ChangeMeshComp->SetupAttachment(ParentComp);
+	OriginMeshComp->SetupAttachment(ParentComp);
 }
 
 void AFlowerActor::BeginPlay()
 {
 	Super::BeginPlay();
 
+	NiagaraComp->SetActive(false);
 	ChangeMeshComp->SetVisibility(false);
 
 	TriggerComp->OnComponentBeginOverlap.AddDynamic(this, &AFlowerActor::OverlapBegin);
@@ -23,6 +29,7 @@ void AFlowerActor::BeginPlay()
 void AFlowerActor::OverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	TriggerComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	NiagaraComp->SetActive(true);
 	ChangeMeshComp->SetVisibility(true);
 	OriginMeshComp->SetVisibility(false);
 }
