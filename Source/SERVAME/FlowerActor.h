2f30144139126df2aa63b44e362a2f662533901a
parkjin <104075813+staronthepark@users.noreply.github.com>
2023-09-06 16:54:34 +0900

    .

---
+++ Source/SERVAME/FlowerActor.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/BoxComponent.h"
+#include "FlowerActor.generated.h"
+
+UCLASS()
+class SERVAME_API AFlowerActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AFlowerActor();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+
+	UPROPERTY(Editanywhere)
+		UBoxComponent* TriggerComp;
+
+	UPROPERTY(Editanywhere)
+		UStaticMeshComponent* ChangeMeshComp;
+
+	UPROPERTY(Editanywhere)
+		UStaticMeshComponent* OriginMeshComp;
+
+
+	UFUNCTION()
+		void OverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+};
