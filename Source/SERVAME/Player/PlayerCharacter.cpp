854927a0c528580c98cdb82690013027692c3c57
parkjin <104075813+staronthepark@users.noreply.github.com>
2023-09-09 23:27:25 +0900

    .

--- Source/SERVAME/Player/PlayerCharacter.cpp
+++ Source/SERVAME/Player/PlayerCharacter.cpp
@@ -2443,7 +2443,7 @@ void APlayerCharacter::OnParryingOverlapBegin(UPrimitiveComponent* OverlappedCom
 void APlayerCharacter::OnShieldOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	if (IsGrab)return;
-	
+	if (IsExecute) return;
 	ExecutionCharacter = Cast<ABaseCharacter>(OtherActor);
 
 	if (ExecutionCharacter == nullptr)return;
