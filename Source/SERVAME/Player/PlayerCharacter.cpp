60b4a0268bc65b351d6e727acefa6c4154eae853
parkjin <104075813+staronthepark@users.noreply.github.com>
2023-10-04 16:02:29 +0900

    .

--- Source/SERVAME/Player/PlayerCharacter.cpp
+++ Source/SERVAME/Player/PlayerCharacter.cpp
@@ -1792,8 +1792,8 @@ void APlayerCharacter::BeginPlay()
 		}
 	}
 	PlayerDataStruct.SoulCount = 0;
-	GetWorldTimerManager().SetTimer(DeadTimer, this, &APlayerCharacter::LoadFile, 0.2f);
-	GetWorldTimerManager().SetTimer(SprintEndTimer, this, &APlayerCharacter::LoadMap, 0.5f);
+	//GetWorldTimerManager().SetTimer(DeadTimer, this, &APlayerCharacter::LoadFile, 0.2f);
+	//GetWorldTimerManager().SetTimer(SprintEndTimer, this, &APlayerCharacter::LoadMap, 0.5f);
 	ASoundManager::GetInstance().Init();
 	CanShieldDeploy = true;
 	CanUseSkill = true;
