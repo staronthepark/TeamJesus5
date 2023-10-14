2364bbd4d79675a6303104dc86b4bf9a787960bd
parkjin <104075813+staronthepark@users.noreply.github.com>
2023-10-15 00:44:50 +0900

    .

--- Source/SERVAME/LevelLightingManager.cpp
+++ Source/SERVAME/LevelLightingManager.cpp
@@ -1,27 +1,54 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "LevelLightingManager.h"
 
-// Sets default values
 ALevelLightingManager::ALevelLightingManager()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
-// Called when the game starts or when spawned
 void ALevelLightingManager::BeginPlay()
 {
 	Super::BeginPlay();
+
+	DirectionalLightComp
+		= Cast<UDirectionalLightComponent>(DirectionalLight->GetComponentByClass(UDirectionalLightComponent::StaticClass()));
 	
+	ExponentialHeightFogComp
+		= Cast<UExponentialHeightFogComponent>(ExponentialHeightFog->GetComponentByClass(UExponentialHeightFogComponent::StaticClass()));
+	
+	SkyLightComp
+		= Cast<USkyLightComponent>(SkyLight->GetComponentByClass(USkyLightComponent::StaticClass()));
+
+	for (int32 i = 0; i < LightSetting.Num(); i++)
+	{
+		LightSettingMap.Add(LightSetting[i].MapName, LightSetting[i]);
+	}
 }
 
-// Called every frame
 void ALevelLightingManager::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	DirectionalLightComp->SetIntensity(FMath::Lerp(DirectionalLightComp->Intensity, TargetDirectionalLight.Intensity, FadeSpeed * DeltaTime));
+	DirectionalLightComp->SetLightColor(FMath::Lerp(DirectionalLightComp->GetLightColor(), TargetDirectionalLight.LightColor, FadeSpeed * DeltaTime));
+	
+	ExponentialHeightFogComp->SetFogDensity(FMath::Lerp(ExponentialHeightFogComp->FogDensity, TargetHeightFog.FogDensity, FadeSpeed * DeltaTime));
+	ExponentialHeightFogComp->SetFogHeightFalloff(FMath::Lerp(ExponentialHeightFogComp->FogHeightFalloff, TargetHeightFog.FogHeightFallOff, FadeSpeed * DeltaTime));
+	ExponentialHeightFogComp->SetVolumetricFogScatteringDistribution(FMath::Lerp(ExponentialHeightFogComp->VolumetricFogScatteringDistribution, TargetHeightFog.ScatteringDistribution, FadeSpeed * DeltaTime));
+	ExponentialHeightFogComp->SetVolumetricFogEmissive(FMath::Lerp(ExponentialHeightFogComp->VolumetricFogEmissive, TargetHeightFog.Emissive, FadeSpeed * DeltaTime));
+
+	float Red   = FMath::Lerp(ExponentialHeightFogComp->VolumetricFogAlbedo.R, TargetHeightFog.Albedo.R, FadeSpeed * DeltaTime);
+	float Green = FMath::Lerp(ExponentialHeightFogComp->VolumetricFogAlbedo.G, TargetHeightFog.Albedo.G, FadeSpeed * DeltaTime);
+	float Blue  = FMath::Lerp(ExponentialHeightFogComp->VolumetricFogAlbedo.B, TargetHeightFog.Albedo.B, FadeSpeed * DeltaTime);
+
+	ExponentialHeightFogComp->SetVolumetricFogAlbedo(FColor(Red, Green, Blue));
+	
+	SkyLightComp->SetIntensity(FMath::Lerp(SkyLightComp->Intensity, TargetSkyLight.IntensityScale, FadeSpeed * DeltaTime));
+	SkyLightComp->SetLightColor(FMath::Lerp(SkyLightComp->GetLightColor(), TargetSkyLight.LightColor, FadeSpeed * DeltaTime));
 }
 
+void ALevelLightingManager::ChangeTargetLightSetting(FString Name)
+{
+	TargetDirectionalLight = LightSettingMap[Name].DirectionalLight;
+	TargetHeightFog = LightSettingMap[Name].HeightFog;
+	TargetSkyLight = LightSettingMap[Name].SkyLight;
+}
--- Source/SERVAME/LevelLightingManager.h
+++ Source/SERVAME/LevelLightingManager.h
@@ -1,10 +1,68 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Engine/DirectionalLight.h"
+#include "Engine/SkyLight.h"
+#include "Engine/ExponentialHeightFog.h"
+#include "Components/DirectionalLightComponent.h"
+#include "Components/ExponentialHeightFogComponent.h"
+#include "Components/SkyLightComponent.h"
 #include "LevelLightingManager.generated.h"
+
+USTRUCT(BlueprintType)
+struct FMyDirectionalLight
+{
+	GENERATED_BODY()
+
+		UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float Intensity;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		FColor LightColor;
+};
+
+USTRUCT(BlueprintType)
+struct FMYExponentialHeightFog
+{
+	GENERATED_BODY()
+
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ExponentialHeightFog")
+		float FogDensity;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ExponentialHeightFog")
+		float FogHeightFallOff;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Volumetric")
+		float ScatteringDistribution;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Volumetric")
+		FColor Albedo;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Volumetric")
+		FLinearColor Emissive;
+};
+
+USTRUCT(BlueprintType)
+struct FMySkyLight
+{
+	GENERATED_BODY()
+
+		UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float IntensityScale;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		FColor LightColor;
+};
+
+USTRUCT(BlueprintType)
+struct FLightSetting
+{
+	GENERATED_BODY()
+		UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		FString MapName;
+		UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		FMyDirectionalLight DirectionalLight;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		FMYExponentialHeightFog HeightFog;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		FMySkyLight SkyLight;
+};
 
 UCLASS()
 class SERVAME_API ALevelLightingManager : public AActor
@@ -12,15 +70,34 @@ class SERVAME_API ALevelLightingManager : public AActor
 	GENERATED_BODY()
 	
 public:	
-	// Sets default values for this actor's properties
 	ALevelLightingManager();
+		
+	UPROPERTY(Editanywhere)
+		float FadeSpeed;
+
+	UPROPERTY(Editanywhere)
+		TArray<FLightSetting> LightSetting;
+
+	TMap<FString, FLightSetting>LightSettingMap;
+
+	FMyDirectionalLight TargetDirectionalLight;
+	FMYExponentialHeightFog TargetHeightFog;
+	FMySkyLight TargetSkyLight;
+
+	ADirectionalLight* DirectionalLight;
+	AExponentialHeightFog* ExponentialHeightFog;
+	ASkyLight* SkyLight;
+
+	UDirectionalLightComponent* DirectionalLightComp;
+	UExponentialHeightFogComponent* ExponentialHeightFogComp;
+	USkyLightComponent* SkyLightComp;
+
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
+	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaTime) override;
 
+	void ChangeTargetLightSetting(FString Name);
 };
