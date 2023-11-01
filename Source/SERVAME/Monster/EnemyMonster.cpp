6fcd972febcc974030bce3b9943743dfa7606f35
RuuNee <bio8617@gmail.com>
2023-11-01 12:52:14 +0900

    .

--- Source/SERVAME/Monster/EnemyMonster.cpp
+++ Source/SERVAME/Monster/EnemyMonster.cpp
@@ -601,7 +601,10 @@ void AEnemyMonster::MonsterLog(int id, int i)
 	if (MonsterID == id)
 		UE_LOG(LogTemp, Warning, TEXT("%d"), i);
 }
-
+
+void AEnemyMonster::PlayMonsterSoundInPool(EMonsterAudioType AudioType)
+{
+}
 void AEnemyMonster::OnTargetDetectionBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	PlayerCharacter = Cast<APlayerCharacter>(OtherActor);
