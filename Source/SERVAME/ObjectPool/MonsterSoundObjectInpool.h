86dd0be68904fff33879a1ef73a55777c639bec1
RuuNee <bio8617@gmail.com>
2023-11-01 12:29:31 +0900

    .

---
+++ Source/SERVAME/ObjectPool/MonsterSoundObjectInpool.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SoundObjectInPool.h"
+#include "MonsterSoundObjectInpool.generated.h"
+
+/**
+ * 
+ */
+UENUM(BlueprintType)
+enum class EMonsterAudioType : uint8
+{
+	NONE,
+	Knight_Attack,
+	Knight_Die,
+};
+
+
+UCLASS()
+class SERVAME_API AMonsterSoundObjectInpool : public ASoundObjectInPool
+{
+	GENERATED_BODY()
+
+public:
+	AMonsterSoundObjectInpool();
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float LifeTime;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EMonsterAudioType AudioType;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TMap<EMonsterAudioType, USoundBase*> SoundMap;
+
+	virtual void SetActive(bool active) override;
+	virtual void BeginPlay()override;
+	virtual void ReturnObject()override;
+	void PlayMonsterSound(EMonsterAudioType Type);
+};
